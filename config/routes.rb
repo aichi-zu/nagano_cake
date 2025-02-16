1792e04d81ba220421513b9d1ecffdc4eba46c08
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-02-16 14:45:15 +0000

    add sign_up and sign_in for Admin and Customer.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :customers
+
+  # for Admin
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # for Customers
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

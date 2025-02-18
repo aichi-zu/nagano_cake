434078837622b521cddd2e175481b9855243e4d3
abedaiki <abe12261994@gmail.com>
2025-02-18 12:45:05 +0000

    商品新規登録のビュー、モデル、コントローラの修正、商品詳細のビューの修正、画像のサイズ変更のGemの導入

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,4 +7,25 @@ class Admin::ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @tax_included_price = (@item.price_excluding_tax * 1.1).floor
   end
+
+  def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item), notice: "商品を登録しました"
+    else
+      @genres = Genre.all
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price_excluding_tax, :is_sale, :image)
+  end
 end

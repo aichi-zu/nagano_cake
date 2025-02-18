2eb0e76ffa32805e5b973e7ff5d0534f9b5602c3
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-02-18 16:22:48 +0000

    add genre method prototype.

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,38 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @genre_new = Genre.new
+    @genre_all = Genre.all
+  end
+
+  def create
+    @genre_new = Genre.new(genre_params)
+
+    if @genre_new.save
+      redirect_to admin_genres_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @genre_edit_target = Genre.find(params[:id])
+  end
+
+  def update
+    @genre_update_target = Genre.find(params[:id])
+
+    if @genre_update_target.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
+  end
+
+  private
+    def genre_params
+      params.require(:genre).permit(:name)
+    end
+
 end

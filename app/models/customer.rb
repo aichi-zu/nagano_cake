483158e8ad8e1655d879bbeb6c6e61434dd01469
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-02-19 03:22:21 +0000

    【update】マイページ作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,5 +6,9 @@ class Customer < ApplicationRecord
 
   has_many :cart_items, dependent: :destroy
   has_many :addresses
+
+  def active?
+    self.is_active  # もしくは直接 `self.is_active == true` などで判定する
+  end
   
 end

6bd8e674b2be8790a21b2a7c27686eb49fdbd9b2
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-02-20 07:32:54 +0000

    [update]商品一覧ページデザイン更新

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,12 @@
 class Public::ItemsController < ApplicationController
 
   def index
-      @items = Item.all
+    @items = Item.where(is_sale: true).page(params[:page]).per(8)
+    @total_item_count = Item.where(is_sale: true).count
     if current_customer
       @cart_items = CartItem.where(customer_id: current_customer.id)
     else
-        @cart_items = []
+      @cart_items = []
     end
   end
 

eb0d86faca749f39ea51774e5fd50985ce29e045
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-02-20 19:59:18 +0000

    add status auto update method.

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,8 +7,16 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order_update_target = Order.find(params[:id])
+    @order_details_index = OrderDetail.where(order_id: params[:id])
 
     if @order_update_target.update(order_params)
+      # 注文ステータスに応じて、製作ステータスを更新
+      if params[:order][:status] == "confirm_payment"
+        @order_details_index.each do |order_detail|
+          i = OrderDetail.find(order_detail.id)
+          i.update_attribute(:status, "wait_production")
+        end
+      end
       redirect_to request.referer, notice: "注文ステータスを更新しました"
     else
       render :show

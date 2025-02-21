71e5fef0ce71616c545a85edffdf1894775de08a
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-02-21 01:11:16 +0000

    [update]ヘッダーに会員名表示・検索フォーム追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,7 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    @customer = current_customer
     @items = Item.where(is_sale: true).page(params[:page]).per(8)
     @total_item_count = Item.where(is_sale: true).count
     if current_customer
@@ -11,6 +12,7 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
+    @customer = current_customer
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
